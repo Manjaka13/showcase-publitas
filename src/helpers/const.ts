68b2887c0132d6bb4fd930956721cabf2bc517e4
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-22 19:52:54 +0300

    add const file

---
+++ src/helpers/const.ts
@@ -0,0 +1,20 @@
+import { NavigationLinkType } from "@/components/NavigationLinks";
+
+/**
+ * Our constants lies here
+ */
+
+export const NAVIGATION_LINKS: NavigationLinkType[] = [
+  {
+    title: "My work",
+    icon: "wrench",
+  },
+  {
+    title: "Contacts",
+    icon: "envelope",
+  },
+  {
+    title: "Get my resume",
+    icon: "download",
+  },
+];
