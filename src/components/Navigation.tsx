55c14ec60af5253ee05e53c7977e0ecad089c509
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-22 20:24:00 +0300

    fix navigation styles and structure

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,15 +7,17 @@ import { NavigationLinks } from "@/components/NavigationLinks";
 
 export const Navigation = (): JSX.Element => (
 	<nav className="navigation">
-		<div className="navigation__title">
-			<div className="icon-box">
-				<FontAwesomeIcon icon="heart" />
+		<div className="container">
+			<div className="navigation__title">
+				<div className="icon-box">
+					<FontAwesomeIcon icon="heart" />
+				</div>
+				<h1 className="title regular">
+					Hari in <span className="wonderland">Wonderland</span>
+					<FontAwesomeIcon className="star" icon="star-and-crescent" />
+				</h1>
 			</div>
-			<h1 className="title regular">
-				Hari in <span className="wonderland">Wonderland</span>
-				<FontAwesomeIcon className="star" icon="star-and-crescent" />
-			</h1>
+			<NavigationLinks />
 		</div>
-		<NavigationLinks />
 	</nav>
 );
