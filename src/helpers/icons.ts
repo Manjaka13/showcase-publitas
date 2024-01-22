1045552337715160c67b1c1fc12eff2c9823e238
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-22 19:52:50 +0300

    setup base icons

---
+++ src/helpers/icons.ts
@@ -0,0 +1,19 @@
+/**
+ * Import icons into fontawesome library
+ */
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHeart,
+  faStarAndCrescent,
+  faWrench,
+  faDownload,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Manually add fontawesome styles
+config.autoAddCss = false;
+
+// Add icons to library
+library.add(faHeart, faStarAndCrescent, faWrench, faDownload, faEnvelope);
