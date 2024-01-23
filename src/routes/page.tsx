dcf4a978aa4f4c5897b57914cc8c76f0495a982b
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-23 17:06:41 +0300

    cleanup

--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -5,6 +5,7 @@ import "@/helpers/icons";
 import LOGO from "@/images/favicon.png";
 import { Footer } from "@/components/Footer";
 import { Cover } from "@/components/Cover";
+import { ProjectList } from "@/components/ProjectList";
 
 /**
  * Entry point
@@ -21,13 +22,7 @@ const Index = () => (
 			<Navigation />
 			<Cover />
 			<div className="container">
-				<p>
-					Esse veniam culpa officia in consectetur. Eiusmod cillum et officia elit
-					sit dolor labore ea ad nulla. Pariatur labore esse ut consequat laboris
-					exercitation velit cillum aliquip duis. Minim ullamco est incididunt
-					officia minim occaecat aliquip aliquip ullamco enim occaecat officia
-					nostrud deserunt.
-				</p>
+				<ProjectList />
 			</div>
 		</main>
 		<Footer />
