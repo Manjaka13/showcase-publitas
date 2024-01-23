749e93981b4fd3ba8d828f665d7c903d4f7d1d13
manjaka13 <manjaka.rajaonson@gmail.com>
2024-01-23 18:15:19 +0300

    fix card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { ProjectTag, ProjectTagType } from "@/components/ProjectTag";
 
 /**
  * Card that displays a work
@@ -9,8 +11,6 @@ type ProjectLinkType = {
 	github?: string;
 	live: string;
 };
-
-type ProjectTagType = "react" | "nextjs";
 
 export interface ProjectType {
 	name: string;
@@ -26,52 +26,59 @@ export const ProjectCard: FC<ProjectType> = ({
 	name,
 	date,
 	link,
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	tag,
 	description,
 	image,
 	logo,
-}): JSX.Element => (
-	<div className="project-card">
-		<div
-			className="project-card__head"
-			style={{
-				background: `url(${image})`,
-				backgroundSize: "cover",
-			}}
-		>
-			<p className="date">{date}</p>
-		</div>
-		<div className="project-card__body">
-			<div className="project-card__title">
-				<div className="title">
-					{logo && <img className="logo" src={logo} alt="Logo" />}
-					<h3 className="text">{name}</h3>
+}): JSX.Element => {
+	const mappedTechno: JSX.Element[] = tag.map((item: ProjectTagType) => (
+		<li className="item" key={uuidv4()}>
+			<ProjectTag tag={item} />
+		</li>
+	));
+
+	return (
+		<div className="project-card">
+			<div
+				className="project-card__head"
+				style={{
+					background: `url(${image})`,
+					backgroundSize: "cover",
+				}}
+			>
+				<p className="date">{date}</p>
+			</div>
+			<div className="project-card__body">
+				<div className="project-card__title">
+					<div className="title">
+						{logo && <img className="logo" src={logo} alt="Logo" />}
+						<h3 className="text">{name}</h3>
+					</div>
+					{link.github && (
+						<a
+							className="repo-link regular"
+							href={link.github}
+							title="View repository"
+						>
+							<FontAwesomeIcon icon={["fab", "github"]} /> Repo
+						</a>
+					)}
 				</div>
-				{link.github && (
+				<div className="project-card__divider"></div>
+				<ul className="project-card__techno">{mappedTechno}</ul>
+				<p className="project-card__description" title={description}>
+					{description}
+				</p>
+				{link.live && (
 					<a
-						className="repo-link regular"
-						href={link.github}
-						title="View repository"
+						className="button project-card__view regular"
+						href={link.live}
+						title="View live"
 					>
-						<FontAwesomeIcon icon={["fab", "github"]} /> Repo
+						<FontAwesomeIcon icon="external-link" /> Visit
 					</a>
 				)}
 			</div>
-			<ul className="project-card__techno"></ul>
-			<div className="project-card__divider"></div>
-			<p className="project-card__description" title={description}>
-				{description}
-			</p>
-			{link.live && (
-				<a
-					className="button project-card__view regular"
-					href={link.live}
-					title="View live"
-				>
-					<FontAwesomeIcon icon="external-link" /> Visit
-				</a>
-			)}
 		</div>
-	</div>
-);
+	);
+};
